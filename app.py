6bb4cc6d8b2c60da1261a8e58772e4ba16a48707
Ballred95 <blake.allred95@icloud.com>
2020-07-22 11:14:13 -0600

    added imgsrc

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
@@ -18,22 +18,39 @@ CORS(app)
 class Save(db.model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=False)
+    imgsrc = db.Column(db.String(), unique=False)
     text_content = db.Column(db.String(100), unique=False)
     text_align = db.Column(db.String(100), unique=False)
     clipart = db.Column(db.String(100), unique=False)
 
-    def __init__(self, name, text_content, text_align, clipart):
+    def __init__(self, name, imgsrc, text_content, text_align, clipart):
         self.name = name
+        self.imgsrc = imgsrc
         self.text_content = text_content
         self.text_align = text_align
         self.clipart = clipart
 
 class SaveSchema(ma.Schema):
     class Meta:
-        fields = ('name', 'text_content', 'text-align', 'clipart')
+        fields = ('name', 'imgsrc', 'text_content', 'text-align', 'clipart')
 
 save_schema = SaveSchema()
 saves_schema = SaveSchema(many=True)
+
+@app.route('/saved', methods=["POST"])
+def add_save():
+    name = request.json['name']
+    imgsrc = request.json['imgsrc']
+    text_content = request.json['text_content']
+    text_align = request.json['text_align']
+    clipart = request.json['clipart']
+
+    new_save = Save(name, imgsrc, text_content, text_align, clipart)
+
+    db.session.add(new_save)
+    db.session.commit()
+
+    save = Save.query.get(new_guide.id)
 
 
 if __name__ == "__main__":
